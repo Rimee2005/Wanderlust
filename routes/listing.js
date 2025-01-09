ec353b82dcbe3c47d358defb94af92a644c6ec8e
Rimjhim <rimjhimjha961@gmail.com>
2025-01-09 20:34:58 +0530

    "Re-style Rating " And "added Upload Image Option by manuplating form"

--- routes/listing.js
+++ routes/listing.js
@@ -4,16 +4,21 @@ const wrapAsync = require("../utils/wrapAsync.js")
 const Listing = require("../models/listing.js")
 const{isLoggedIn , isOwner , ValidateListing} = require("../middleware.js");
 const { index } = require("../controllers/listings.js");
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 const listingController = require("../controllers/listings.js")
 
  router
   .route("/")
   .get( wrapAsync(listingController.index))
-  .post(
-    isLoggedIn,
-    ValidateListing, 
-    wrapAsync(listingController.createListing) );
+//   .post(
+//     isLoggedIn,
+//     ValidateListing, 
+//     wrapAsync(listingController.createListing),
+    .post(upload.single('listing[image]') , (req , res) => {
+        res.send(req.file)
+    });
 
 //New Route
 router.get("/new" ,  isLoggedIn , listingController.renderNewForm );
